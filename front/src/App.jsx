3f314e90ef2e9dcac8dc0969610224c00d538b83
kuollivo64 <kuollivo64@gmail.com>
2023-07-15 18:08:08 -0400

    Init My Unplash

---
+++ front/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div>NavBar</div>
+      <main>Content</main>
+      <footer>Kuollivo64</footer>
+    </div>
+  );
+}
+
+export default App;
